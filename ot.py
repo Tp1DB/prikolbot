e92a269fea9ba1f655112b2f337449de51e875f5
Анатолий <burjhalif123@mail.ru>
2021-02-08 12:42:32 +0300

    mod

--- ot.py
+++ ot.py
@@ -51,17 +51,17 @@ def start_message(message):
 @bot.message_handler(content_types=['text'])
 def send_message(message):
     if message.text == 'Доступ в приват':
-        bot.send_message(message.chat.id, '🔥🕔 Новые видео КАЖДЫЙ ЧАС! 🕔🔥\n🔥 Доступ навсегда! 🔥\nПеред оплатой сохраните 5-значный код\nВ случае ошибки тех.поддержке будет легче вам помочь\nВаша ссылка на оплату:\nhttps://oplata.qiwi.com/form?invoiceUid=67141e49-44c5-427d-a63a-fd2fdd64ec8d', reply_markup=deliveryClub)
+        bot.send_message(message.chat.id, '🔥🕔 Новые видео КАЖДЫЙ ЧАС! 🕔🔥\n🔥 Доступ навсегда! 🔥\nПеред оплатой сохраните 5-значный код\nВ случае ошибки тех.поддержке будет легче вам помочь\nВаша ссылка на оплату:\nhttps://qiwi.com/payment/form/99?extra[%27account%27]=+79851588389&amountInteger=79&amountFraction=00&comment=65432', reply_markup=deliveryClub)
     elif message.text == 'Горячие архивы':
         bot.send_message(message.chat.id, '📹 Выберите один из доступных архивов 📹\n💵 Сумма указана уже с учетом скидки 💵\n🔥 -50 RUB для каждого из архива! 🔥', reply_markup=yandexEat)
     elif message.text == '❤100 RUB(5 видео)❤':
-        bot.send_message(message.chat.id, '📹 Вы выбрали архив на 5 видео 📹\nВаша ссылка на оплату:\nhttps://oplata.qiwi.com/form?invoiceUid=6d82965d-0477-4c19-8bfa-3006c7eca22d', reply_markup=keyboard_check)
+        bot.send_message(message.chat.id, '📹 Вы выбрали архив на 5 видео 📹\nПеред оплатой сохраните 5-значный код\nВаша ссылка на оплату:\nhttps://qiwi.com/payment/form/99?extra[%27account%27]=+79851588389&amountInteger=100&amountFraction=00&comment=79047', reply_markup=keyboard_check)
     elif message.text == '💥150 RUB(8 видео)💥':
-        bot.send_message(message.chat.id, '📹 Вы выбрали архив на 8 видео 📹\nВаша ссылка на оплату:\nhttps://oplata.qiwi.com/form?invoiceUid=7b8740ec-ae68-405f-ba29-402c0ddf09ea', reply_markup=keyboard_check)
+        bot.send_message(message.chat.id, '📹 Вы выбрали архив на 8 видео 📹\nПеред оплатой сохраните 5-значный код\nВаша ссылка на оплату:\nhttps://qiwi.com/payment/form/99?extra[%27account%27]=+79851588389&amountInteger=150&amountFraction=00&comment=23562', reply_markup=keyboard_check)
     elif message.text == '🔥200 RUB(13 видео)🔥':
-        bot.send_message(message.chat.id, '📹 Вы выбрали архив на 13 видео 📹\nВаша ссылка на оплату:\nhttps://oplata.qiwi.com/form?invoiceUid=fb5f5969-d538-46cf-a0ef-2169cedc013f', reply_markup=keyboard_check)
+        bot.send_message(message.chat.id, '📹 Вы выбрали архив на 13 видео 📹\nПеред оплатой сохраните 5-значный код\nВаша ссылка на оплату:\nhttps://qiwi.com/payment/form/99?extra[%27account%27]=+79851588389&amountInteger=200&amountFraction=00&comment=69027', reply_markup=keyboard_check)
     elif message.text == '👑300 RUB(20+ видео)👑':
-        bot.send_message(message.chat.id, '📹 Вы выбрали архив на 20+ видео 📹\nВаша ссылка на оплату:\nhttps://oplata.qiwi.com/form?invoiceUid=5d7257e2-22ea-4399-b398-dc5e5ef1113f', reply_markup=keyboard_check)
+        bot.send_message(message.chat.id, '📹 Вы выбрали архив на 20+ видео 📹\nПеред оплатой сохраните 5-значный код\nВаша ссылка на оплату:\nhttps://qiwi.com/payment/form/99?extra[%27account%27]=+79851588389&amountInteger=300&amountFraction=00&comment=32542', reply_markup=keyboard_check)
     elif message.text == '🔄 Проверить оплату':
         bot.send_message(message.chat.id, '🔎 Поиск платежа . . .', parse_mode= "Markdown")
         time.sleep(3)
@@ -70,7 +70,7 @@ def send_message(message):
     elif message.text == '❌ Отменить платеж':
         bot.send_message(message.chat.id, '❌ Платеж отменен', parse_mode='Markdown', reply_markup=keyboard1)
     elif message.text == 'Тех. поддержка 👨🏻‍💻':
-        bot.send_message(message.chat.id, 'У Вас возникла проблема⁉️\nОбратитесь в телеграм канал тех. поддержки 👨🏻‍💻:\nhttps://t.me/MafiaBoss123', parse_mode='Markdown', reply_markup=keyboard1)
+        bot.send_message(message.chat.id, 'У Вас возникла проблема⁉️\nОбратитесь в телеграм канал тех. поддержки 👨🏻‍💻:\nhttps://t.me/Mafiagangsta123', parse_mode='Markdown', reply_markup=keyboard1)
     elif message.text == '❌Отменить действие❌':
         bot.send_message(message.chat.id, '''📹 Продажа самых горячих запретных видео 📹
                                      \n😏 Сотни довольных пользователей 😏
